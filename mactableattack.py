6a81dd604b9f77c567a46f2d1bc28a60dab580b1
Jeremy Fu <gengf@andrew.cmu.edu>
2014-05-02 21:53:15 -0400

    Fix the topo files so that we can use dpctl command

--- mactableattack.py
+++ mactableattack.py
@@ -7,7 +7,7 @@ from mininet.node import CPULimitedHost
 from mininet.cli import CLI
 from mininet.topo import Topo
 from mininet.node import RemoteController
-
+import time
 
 class MactableAttack(Topo):
     
@@ -24,11 +24,11 @@ class MactableAttack(Topo):
 
 def simpleTest():
     topo=MactableAttack()
-    net = Mininet( topo=topo, controller=partial( RemoteController, ip='10.0.2.2', port=6633 ) )
+    net = Mininet(topo=topo, listenPort=6634, controller = partial(RemoteController, ip='10.0.2.2'))
     net.start()
     h1=net.get('h1')
     h2=net.get('h2')
-    print h1.cmd( 'ping -c1', h2.IP() )
+    #print h1.cmd( 'ping -c1', h2.IP() )
     
     #print "Host", h1.name, "has IP address", h1.IP(), "and MAC address", h1.MAC()
     #print "h1 Mac address:\t" + h1.MAC()                                                            
@@ -38,18 +38,20 @@ def simpleTest():
     #for prefix2 in range(255):
      #   for prefix3 in range(255):
             #print prefix1 + ":" + "%x" % prefix2 + ":" + "%x" % prefix3
-            h1.setMAC(mac1)
-            h1.cmd("ping -c1", h2.IP())
-            h1.setMac(mac2)
-            h1.cmd("ping -c1", h2.IP()) 
-            CLI(net)           
-            h1.setMAC(mac3)
-            h1.cmd("ping -c1", h2.IP())
-            h1.setMAC(mac1)
-            h1.cmd("ping -c1", h2.IP())
-            h1.setMAC(mac3)
-            h1.cmd("ping -c1", h2.IP())   
-    CLI(net)                                                                                                            
+    h1.setMAC(mac1)
+    h1.cmd("ping -c1", h2.IP())
+    h1.setMAC(mac2)
+    h1.cmd("ping -c1", h2.IP()) 
+    CLI(net)
+    #CLI(net)
+    h1.setMAC(mac3)
+    h1.cmd("ping -c1", h2.IP())
+    #h1.setMAC(mac1)
+    #CLI(net)
+    #h1.cmd("ping -c1", h2.IP())
+    #h0.setMAC(mac3)
+    #h1.cmd("ping -c1", h2.IP())   
+    CLI(net)
     net.stop()  
 
 if __name__ == '__main__':
